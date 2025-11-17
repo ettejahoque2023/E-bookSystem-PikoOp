93e7e23e20739bc88bf3c1e4ba000bc019b09816
Etteja Hoque <ettejahoque2003@gmail.com>
2025-11-18 00:40:17 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -72,8 +72,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
 // Error handling middleware (must come *after* routes)
